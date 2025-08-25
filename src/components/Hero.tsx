1c9575e550d5d9251f4143b564b1f3901af8a40f
HamidaBz <hami.bouaziz@gmail.com>
2025-08-25 13:48:56 +0200

    add overlay to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,25 @@
-import '../style/hero.css'
+import "../style/hero.css";
 
-function Hero(){
-    return (
-        <section className="hero d-flex align-items-center text-center min-vh-50 min-vh-sm-75 min-vh-md-100">
-            <div className="hero-overlay"></div>
-            <div className="container overlay-content">
-                <h1 className="display-3 fw-bold"> Bienvenue sur notre site</h1>
-                <p className="mb-4">Ensemble, construisons de beaux projets pour l'avenir </p>
-                <a className="btn btn-primary btn-lg">Découvrir</a>
-                <a className="btn btn-outline-light btn-lg">Faire un don</a>
-            </div>
-        </section>
-    );
+function Hero() {
+  return (
+    <div className="hero ">
+      <header className="hero-header d-flex align-items-center text-center min-vh-50 min-vh-sm-75 min-vh-md-100">
+        <div className="container ">
+          <h1 className="display-3 fw-bold"> Bienvenue sur notre site</h1>
+          <p className="mb-4">
+            Ensemble, construisons de beaux projets pour l'avenir{" "}
+          </p>
+        </div>
+      </header>
+      <div className="hero-overlay shadow d-flex align-items-center justify-content-center">
+        <div className="overlay-content col-5">
+            <a className="btn btn-primary btn-lg">Découvrir</a>
+            <a className="btn btn-outline-light btn-lg">Faire un don</a>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default Hero;
